2fb88eaabdfda46466320a56c47492a9d44df02a
PassyahRaffi <passyah11@gmail.com>
2025-10-13 22:16:30 +0700

    commit

---
+++ src/utils/desturing.tsx
@@ -0,0 +1,13 @@
+// object
+export const object = `
+const user = { name: 'Reva', age: 22 }
+`;
+export const user = { name: "Reva", age: 22 };
+export const { name, age } = user;
+
+// array
+export const array = `
+const colors = ['red', 'green', 'blue']
+`;
+export const colors = ["red", "green", "blue"];
+export const [first, second, third] = colors;
